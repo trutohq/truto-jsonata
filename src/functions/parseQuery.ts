b24d3f92d3091977295f37a1d3272424be68a5dc
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2025-05-27 14:25:23 +0530

    use options

--- src/functions/parseQuery.ts
+++ src/functions/parseQuery.ts
@@ -1,5 +1,8 @@
 import qs from 'qs'
 
-export default function parseQuery(query: string) {
-  return qs.parse(query)
+export default function parseQuery(
+  query: string,
+  options: Record<string, unknown>
+) {
+  return qs.parse(query, options)
 }
--- src/functions/stringifyQuery.ts
+++ src/functions/stringifyQuery.ts
@@ -1,5 +1,8 @@
 import qs from 'qs'
 
-export default function stringifyQuery(query: Record<string, unknown>) {
-  return qs.stringify(query)
+export default function stringifyQuery(
+  query: Record<string, unknown>,
+  options: Record<string, unknown>
+) {
+  return qs.stringify(query, options)
 }
