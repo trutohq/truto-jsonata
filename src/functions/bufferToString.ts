2ae1e07da6a9e79873411ebfb7fc1042b7edf8d0
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2025-12-24 23:34:45 +0530

    Fix types

--- src/functions/bufferToString.ts
+++ src/functions/bufferToString.ts
@@ -1,13 +1,15 @@
+/// <reference types="@cloudflare/workers-types" />
+
 async function bufferToString(
   value: Buffer | ReadableStream,
   encoding: BufferEncoding | undefined
 ) {
   if (value instanceof ReadableStream) {
-    const chunks = []
+    const chunks: Buffer[] = []
     for await (const chunk of value) {
       chunks.push(Buffer.from(chunk))
     }
-    const buffer = Buffer.concat(chunks)
+    const buffer = Buffer.concat(chunks as readonly Uint8Array[])
     return buffer.toString(encoding)
   }
   return value.toString(encoding)
--- src/functions/getDataUri.ts
+++ src/functions/getDataUri.ts
@@ -1,3 +1,5 @@
+/// <reference types="@cloudflare/workers-types" />
+
 async function getDataUri(
   file: Blob | Buffer | ReadableStream,
   mimeType: string
@@ -6,18 +8,18 @@ async function getDataUri(
     throw new Error('Mime type is required')
   }
   if (file instanceof ReadableStream) {
-    const chunks = []
+    const chunks: Buffer[] = []
     for await (const chunk of file) {
       chunks.push(Buffer.from(chunk))
     }
-    const buffer = Buffer.concat(chunks)
+    const buffer = Buffer.concat(chunks as readonly Uint8Array[])
     const base64Image = buffer.toString('base64')
 
     // Construct the data URI for a PNG image
     return `data:${mimeType};base64,${base64Image}`
   }
   const arrayBuffer = file instanceof Blob ? await file.arrayBuffer() : file
-  const buffer = Buffer.from(arrayBuffer)
+  const buffer = Buffer.from(arrayBuffer as ArrayBuffer)
   const base64Image = buffer.toString('base64')
 
   return `data:${mimeType};base64,${base64Image}`
